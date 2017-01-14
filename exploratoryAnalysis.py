32dc724d04d38073f6d9c82fbb5910f1b32937c9
David Rynties <ryntiesd@mail.gvsu.edu>
2017-01-14 16:07:07 -0500

    moved function call to main and formated time check to microseconds

--- exploratoryAnalysis.py
+++ exploratoryAnalysis.py
@@ -13,39 +13,37 @@ wordList = []
 
 filename = '/Users/dave/CIS/678/P1/ga.txt'
 
-def calc_num_syllables(word):
-
-    silent = None
-    num_syllables = 0
-
-    if word.endswith('e'):
-        silent = True
-
-    for index, letter in enumerate(word):
-        if index == 0 and word[index] in vowels:
-            num_syllables += 1
-        elif word[index - 1] not in vowels:
-            if index < len(word) - 1 and word[index] in vowels:
-                num_syllables += 1
-            elif index == len(word) - 1 and word[index] in vowels:
-                num_syllables += 1
-
-    if num_syllables > 1 and silent:
-        num_syllables -= 1
-
-    return num_syllables
-
-
 with open(filename, 'r') as file:
+    start_time = datetime.datetime.now()
     for line in file:
         line.replace("...", " ")
         numSentences += line.count('.')
         line = line.translate(None, string.punctuation)
-        start_time = datetime.datetime.now()
+
+
         for word in line.split():
-            listNumSyllables.append(calc_num_syllables(word))
-        total_time = datetime.datetime.now() - start_time
-        millis = int(total_time.total_seconds() * 1000)
+            silent = None
+            num_syllables = 0
+
+            if word.endswith('e'):
+                silent = True
+
+            for index, letter in enumerate(word):
+                if index == 0 and word[index] in vowels:
+                    num_syllables += 1
+                elif word[index - 1] not in vowels:
+                    if index < len(word) - 1 and word[index] in vowels:
+                        num_syllables += 1
+                    elif index == len(word) - 1 and word[index] in vowels:
+                        num_syllables += 1
+
+            if num_syllables > 1 and silent:
+                num_syllables -= 1
+
+            listNumSyllables.append(num_syllables)
+
+    total_time = datetime.datetime.now() - start_time
+    micro = int(total_time.total_seconds() * 1000000)
 
 totalSyllables = sum(listNumSyllables)
 totalWords = len(listNumSyllables)
@@ -59,8 +57,7 @@ print('\nAll Items for Histogram in: listNumSyllables')
 print('Number of Sentences: %i' % numSentences)
 print('Number of Words: %i' % len(listNumSyllables))
 print('Number of Syllables: %i' % (sum(listNumSyllables)))
-print('Syllable Count Time: %s Seconds' % total_time)
-print('Syllable Count Time: %i Milliseconds' % millis)
+print('Syllable Count Time: %i Microseconds' % micro)
 print('\nFlesch Index: %.2f' % fi)
 
 plt.hist(listNumSyllables)
@@ -68,4 +65,3 @@ plt.title("Syllable Complexity")
 plt.xlabel("Number of Syllables")
 plt.ylabel("Frequency")
 plt.savefig('histogram.png')
-
